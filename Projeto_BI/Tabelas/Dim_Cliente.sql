d8b07019265cc3b37b9d841c7c2f1abd69a3771b
Edvania Evangelista <edvanianascimento@yahoo.com.br>
2022-09-04 15:44:39 -0300

    Adicionar arquivos de projeto.

---
+++ Projeto_BI/Tabelas/Dim_Cliente.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[Dim_Cliente](
+	[Cod_Cliente] NVARCHAR(50) not null primary key,
+	[Desc_Cliente] NVARCHAR(200) NULL, 
+    [Cod_Cidade] NVARCHAR(50) NULL, 
+    [Desc_cidade] NVARCHAR(200) NULL,
+	[Cod_Estado] NVARCHAR(50) NULL, 
+    [Desc_Estado] NVARCHAR(200) NULL,
+	[Cod_Regiao] NVARCHAR(50) NULL, 
+    [Desc_Regiao] NVARCHAR(200) NULL,
+	[Cod_Segmento] NVARCHAR(50) NULL, 
+    [Desc_Segmento] NVARCHAR(200) NULL
+
+)
